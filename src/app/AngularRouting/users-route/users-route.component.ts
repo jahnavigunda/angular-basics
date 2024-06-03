91c17b32eb997feefa746c5318e751ef9167dd28
jahnavigunda <jahnavi.gunda93@gmail.com>
2024-06-02 22:06:50 -0700

    navigate b/w pages using router

--- src/app/AngularRouting/users-route/users-route.component.ts
+++ src/app/AngularRouting/users-route/users-route.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users-route',
   templateUrl: './users-route.component.html',
   styleUrls: ['./users-route.component.css']
 })
-export class UsersRouteComponent {
+export class UsersRouteComponent implements OnInit{
+
+  constructor(private router: Router){}
+  ngOnInit(): void {
+  }
+  onCategoriesClick(){
+    //perform some logic and navigate to page
+    // this.router.navigateByUrl('/categories');
+    this.router.navigate(['/categories']);
+
+  }
 
 }
